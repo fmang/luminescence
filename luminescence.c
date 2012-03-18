5d46613d8e4145fd39ffe5f6a2f1cd4b1978a33b
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-18 21:45:55 +0100

    scrolled window + status label

--- luminescence.c
+++ luminescence.c
@@ -8,10 +8,20 @@ int main(int argc, char **argv){
     gtk_window_set_has_resize_grip(GTK_WINDOW(window), FALSE);
     g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
+    GtkWidget *layout = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+
     GtkWidget *web_view = webkit_web_view_new();
-    gtk_container_add(GTK_CONTAINER(window), web_view);
     webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), "file:///");
 
+    GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(scrolled_window), web_view);
+    gtk_box_pack_start(GTK_BOX(layout), scrolled_window, TRUE, TRUE, 0);
+
+    GtkWidget *status = gtk_label_new("Hello world!");
+    gtk_misc_set_alignment(GTK_MISC(status), 0, 0.5);
+    gtk_box_pack_start(GTK_BOX(layout), status, FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(window), layout);
     gtk_widget_show_all(window);
     gtk_main();
 
