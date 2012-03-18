92149d4e4e0e5668dd6ebfbca891afbfaf9c0e65
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-18 21:03:18 +0100

    webkit view

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -g -Wall -o luminescence luminescence.c `pkg-config --cflags --libs gtk+-3.0`
+gcc -g -Wall -o luminescence luminescence.c `pkg-config --cflags --libs gtk+-3.0 webkitgtk-3.0`
--- luminescence.c
+++ luminescence.c
@@ -1,10 +1,18 @@
 #include <gtk/gtk.h>
+#include <webkit/webkit.h>
 
 int main(int argc, char **argv){
     gtk_init(&argc, &argv);
+
     GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(win, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_show(win);
+
+    GtkWidget *web = webkit_web_view_new();
+    gtk_container_add(GTK_CONTAINER(win), web);
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web), "file:///");
+
+    gtk_widget_show_all(win);
     gtk_main();
+
     return 0;
 }
