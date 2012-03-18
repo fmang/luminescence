4130c1c19cedc4cfdfdc8e3cd08244a66653e430
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-18 21:15:02 +0100

    longer names
    gtk way

--- luminescence.c
+++ luminescence.c
@@ -4,15 +4,15 @@
 int main(int argc, char **argv){
     gtk_init(&argc, &argv);
 
-    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_has_resize_grip(GTK_WINDOW(win), FALSE);
-    g_signal_connect(win, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_has_resize_grip(GTK_WINDOW(window), FALSE);
+    g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
-    GtkWidget *web = webkit_web_view_new();
-    gtk_container_add(GTK_CONTAINER(win), web);
-    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web), "file:///");
+    GtkWidget *web_view = webkit_web_view_new();
+    gtk_container_add(GTK_CONTAINER(window), web_view);
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), "file:///");
 
-    gtk_widget_show_all(win);
+    gtk_widget_show_all(window);
     gtk_main();
 
     return 0;
