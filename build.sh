67fac01022171c1b664389b39dff9aa8f417f969
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-24 17:13:09 +0100

    lumi-install script
    compiles and install a C plugin
    build.sh doesn't compile plugins anymore

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 gcc -g -Wall -o luminescence luminescence.c -ldl `pkg-config --cflags --libs gtk+-3.0 webkitgtk-3.0`
-cd plugins
-for f in *.c ; do
-    gcc -g -Wall -shared -o "$(basename "$f" .c).so" "$f" -fPIC `pkg-config --cflags --libs gtk+-3.0 webkitgtk-3.0`
-done
