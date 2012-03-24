80a2f814acb081a3776e72fdcf68ed904e42a6c7
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-24 15:38:43 +0100

    little cleanup

--- luminescence.c
+++ luminescence.c
@@ -28,59 +28,59 @@ bool on_key_press(GtkWidget *widget, GdkEventKey *event){
 }
 
 void load_plugin(const char *path){
-    if(key_callback_count >= key_callbacks_size){
-        key_callbacks_size += 8;
-        key_callbacks = (KeyCallback**) realloc(key_callbacks, key_callbacks_size*sizeof(KeyCallback*));
-    }
     void *plugin = dlopen(path, RTLD_LAZY);
     if(!plugin) return;
+
+    // Init
     void (*plugin_init)(Lumi*) = dlsym(plugin, "init");
-    if(plugin_init)
-        (*plugin_init)(&lumi);
+    if(plugin_init) (*plugin_init)(&lumi);
+
+    // Key callback
     KeyCallback* plugin_key_callback = dlsym(plugin, "key_callback");
-    if(plugin_key_callback)
+    if(plugin_key_callback){
+        if(key_callback_count >= key_callbacks_size){
+            // Grow key_callbacks
+            key_callbacks_size += 8;
+            key_callbacks = (KeyCallback**) realloc(key_callbacks, key_callbacks_size*sizeof(KeyCallback*));
+        }
         key_callbacks[key_callback_count++] = plugin_key_callback;
+    }
 }
 
 int main(int argc, char **argv){
     gtk_init(&argc, &argv);
 
     // Window
-
     lumi.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_add_events(lumi.window, GDK_KEY_PRESS_MASK);
     gtk_window_set_has_resize_grip(GTK_WINDOW(lumi.window), FALSE);
     g_signal_connect(lumi.window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(lumi.window, "key-press-event", G_CALLBACK(on_key_press), NULL);
 
+    // Layout
     GtkWidget *layout = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(lumi.window), layout);
     gtk_widget_show(layout);
 
     // Web view
-
     lumi.web_view = webkit_web_view_new();
-
     GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER(scrolled_window), lumi.web_view);
     gtk_box_pack_start(GTK_BOX(layout), scrolled_window, TRUE, TRUE, 0);
     gtk_widget_show_all(scrolled_window);
 
-    // Status
-
+    // Status bar
     lumi.status_bar = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_box_pack_start(GTK_BOX(layout), lumi.status_bar, FALSE, FALSE, 0);
     gtk_widget_show(lumi.status_bar);
 
+    // Plugins
     load_plugin("plugins/reload.so");
     load_plugin("plugins/address-bar.so");
     load_plugin("plugins/scripts-toggler.so");
     load_plugin("plugins/insert.so");
 
-    // Show
-
-    gtk_container_add(GTK_CONTAINER(lumi.window), layout);
     gtk_widget_show(lumi.window);
     gtk_main();
-
     return 0;
 }
