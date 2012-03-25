8418bed9520e80254e15bc1f23df1e487e3d5444
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-25 16:18:03 +0200

    scripts-toggler: options

--- plugins/scripts-toggler.c
+++ plugins/scripts-toggler.c
@@ -9,10 +9,18 @@ void scripts_set_enabled(gboolean enable){
     gtk_widget_set_visible(scripts_label, enable);
 }
 
+void enable_scripts(){ scripts_set_enabled(TRUE); }
+void disable_scripts(){ scripts_set_enabled(FALSE); }
+
+Option options[] = {
+    { "scripts", NO_ARGUMENT, enable_scripts, "Enable scripts by default." },
+    { "no-scripts", NO_ARGUMENT, disable_scripts, "Disable scripts (default)." },
+    { 0 } };
+
 int key_callback(GdkEventKey *e){
     switch(e->keyval){
-        case GDK_KEY_s: scripts_set_enabled(FALSE); return EVENT_STOP;
-        case GDK_KEY_S: scripts_set_enabled(TRUE);  return EVENT_STOP;
+        case GDK_KEY_s: enable_scripts();  return EVENT_STOP;
+        case GDK_KEY_S: disable_scripts(); return EVENT_STOP;
     }
     return EVENT_PROPAGATE;
 }
@@ -21,5 +29,5 @@ void init(Lumi *l){
     web_view = l->web_view;
     scripts_label = gtk_label_new("JS");
     gtk_box_pack_start(GTK_BOX(l->status_bar), scripts_label, FALSE, FALSE, 3);
-    scripts_set_enabled(FALSE);
+    disable_scripts();
 }
