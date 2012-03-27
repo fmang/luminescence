3b13ec99e7aff61f589d0dbfdcd2f980021b068d
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-27 19:54:16 +0200

    insert plugin: doc

--- plugins/insert.c
+++ plugins/insert.c
@@ -1,5 +1,9 @@
 #include <luminescence.h>
 
+const char *name = "Insert mode";
+
+const char *description = "Press i to enter insert mode. Escape to leave.";
+
 GtkWidget *insert_label;
 
 int key_callback(GdkEventKey *e){
@@ -8,7 +12,7 @@ int key_callback(GdkEventKey *e){
         return FOCUS_RELEASE | EVENT_PROPAGATE;
     }
     else if(gtk_widget_get_visible(insert_label))
-        return FOCUS_GRAB | EVENT_PROPAGATE; // propagate to WebKit
+        return FOCUS_GRAB | EVENT_PROPAGATE;
     else if(e->keyval == GDK_KEY_i){
         gtk_widget_show(insert_label);
         return FOCUS_GRAB | EVENT_STOP;
@@ -18,5 +22,5 @@ int key_callback(GdkEventKey *e){
 
 void init(Lumi *lumi){
     insert_label = gtk_label_new("INSERT");
-    gtk_box_pack_start(GTK_BOX(lumi->status_bar), insert_label, FALSE, FALSE, 3);
+    gtk_box_pack_end(GTK_BOX(lumi->status_bar), insert_label, FALSE, FALSE, 3);
 }
