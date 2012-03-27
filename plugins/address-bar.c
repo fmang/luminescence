c6d30cf5266d3160f039585505fa3400ab0caa56
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-27 19:39:19 +0200

    split address-bar
    address-bar (key o) contains only the entry
    uri-viewer (key u) displays the current/hovered URI

--- plugins/address-bar.c
+++ plugins/address-bar.c
@@ -3,22 +3,10 @@
 #include <string.h>
 
 GtkWidget *web_view;
-
-GtkWidget *address_label;
-GtkWidget *address_entry;
-
-void update_address_label(){
-    if(webkit_web_view_get_load_status(WEBKIT_WEB_VIEW(web_view)) == WEBKIT_LOAD_COMMITTED)
-        gtk_label_set_text(GTK_LABEL(address_label), webkit_web_view_get_uri(WEBKIT_WEB_VIEW(web_view)));
-}
-
-void display_hovered_uri(WebKitWebView *view, gchar *title, gchar *uri){
-    gtk_label_set_text(GTK_LABEL(address_label),
-        uri ? uri : webkit_web_view_get_uri(view));
-}
+GtkWidget *entry;
 
 void go(){
-    const gchar *raw_uri = gtk_entry_get_text(GTK_ENTRY(address_entry));
+    const gchar *raw_uri = gtk_entry_get_text(GTK_ENTRY(entry));
     gchar *uri = (gchar*) malloc(strlen(raw_uri)+8);
     uri[0] = '\0';
     if(!strstr(raw_uri, "://"))
@@ -26,25 +14,21 @@ void go(){
     strcat(uri, raw_uri);
     webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), uri);
     free(uri);
-    gtk_widget_hide(address_entry);
-    gtk_widget_show(address_label);
+    gtk_widget_hide(entry);
 }
 
 int key_callback(GdkEventKey *e){
     if(e->keyval == GDK_KEY_Escape){
-        gtk_widget_hide(address_entry);
-        gtk_widget_show(address_label);
+        gtk_widget_hide(entry);
         return FOCUS_RELEASE | EVENT_PROPAGATE;
     }
-    else if(gtk_widget_get_visible(address_entry))
+    else if(gtk_widget_get_visible(entry))
         return FOCUS_GRAB | EVENT_PROPAGATE;
-    else if(e->keyval == GDK_KEY_u){
-        // Display the entry
+    else if(e->keyval == GDK_KEY_o){
         const gchar *uri = webkit_web_view_get_uri(WEBKIT_WEB_VIEW(web_view));
-        gtk_entry_set_text(GTK_ENTRY(address_entry), uri ? uri : "");
-        gtk_widget_hide(address_label);
-        gtk_widget_show(address_entry);
-        gtk_widget_grab_focus(address_entry);
+        gtk_entry_set_text(GTK_ENTRY(entry), uri ? uri : "");
+        gtk_widget_show(entry);
+        gtk_widget_grab_focus(entry);
         return FOCUS_GRAB | EVENT_STOP;
     }
     return EVENT_PROPAGATE;
@@ -52,19 +36,8 @@ int key_callback(GdkEventKey *e){
 
 void init(Lumi *lumi){
     web_view = lumi->web_view;
-
-    address_label = gtk_label_new("");
-    gtk_label_set_selectable(GTK_LABEL(address_label), TRUE);
-    gtk_misc_set_alignment(GTK_MISC(address_label), 0, 0.5);
-    gtk_misc_set_padding(GTK_MISC(address_label), 2, 2);
-    gtk_box_pack_start(GTK_BOX(lumi->status_bar), address_label, TRUE, TRUE, 0);
-    gtk_widget_show(address_label);
-
-    address_entry = gtk_entry_new();
-    gtk_entry_set_has_frame(GTK_ENTRY(address_entry), FALSE);
-    g_signal_connect(address_entry, "activate", G_CALLBACK(go), NULL);
-    gtk_box_pack_start(GTK_BOX(lumi->status_bar), address_entry, TRUE, TRUE, 0);
-
-    g_signal_connect(web_view, "notify::load-status", G_CALLBACK(update_address_label), NULL);
-    g_signal_connect(web_view, "hovering-over-link", G_CALLBACK(display_hovered_uri), NULL);
+    entry = gtk_entry_new();
+    gtk_entry_set_has_frame(GTK_ENTRY(entry), FALSE);
+    g_signal_connect(entry, "activate", G_CALLBACK(go), NULL);
+    gtk_box_pack_start(GTK_BOX(lumi->status_bar), entry, TRUE, TRUE, 0);
 }
---
+++ plugins/uri-viewer.c
@@ -0,0 +1,35 @@
+#include <luminescence.h>
+
+GtkWidget *uri_label;
+
+void update_uri(GtkWidget *view){
+    if(webkit_web_view_get_load_status(WEBKIT_WEB_VIEW(view)) == WEBKIT_LOAD_COMMITTED)
+        gtk_label_set_text(GTK_LABEL(uri_label), webkit_web_view_get_uri(WEBKIT_WEB_VIEW(view)));
+}
+
+void link_hovered(WebKitWebView *view, gchar *title, gchar *uri){
+    gtk_label_set_text(GTK_LABEL(uri_label), uri ? uri : webkit_web_view_get_uri(view));
+}
+
+int key_callback(GdkEventKey *e){
+    if(e->keyval == GDK_KEY_Escape){
+        gtk_widget_hide(uri_label);
+        return FOCUS_RELEASE | EVENT_PROPAGATE;
+    }
+    else if(gtk_widget_get_visible(uri_label))
+        return FOCUS_GRAB | EVENT_PROPAGATE;
+    else if(e->keyval == GDK_KEY_u){
+        gtk_widget_show(uri_label);
+        return FOCUS_GRAB | EVENT_STOP;
+    }
+    return EVENT_PROPAGATE;
+}
+
+void init(Lumi *lumi){
+    uri_label = gtk_label_new("");
+    gtk_label_set_selectable(GTK_LABEL(uri_label), TRUE);
+    gtk_misc_set_alignment(GTK_MISC(uri_label), 0, 0.5);
+    g_signal_connect(lumi->web_view, "notify::load-status", G_CALLBACK(update_uri), NULL);
+    g_signal_connect(lumi->web_view, "hovering-over-link", G_CALLBACK(link_hovered), NULL);
+    gtk_box_pack_start(GTK_BOX(lumi->status_bar), uri_label, TRUE, TRUE, 0);
+}
