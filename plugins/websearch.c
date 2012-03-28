12177708cd66cfa67c98785822d801774392445b
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-28 19:51:10 +0200

    new websearch plugin

--- plugins/websearch.c
+++ plugins/websearch.c
@@ -2,12 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-GtkWidget *web_view;
+const char *name = "Web Search";
 
+const char *description = "Bind keys to web search engines.";
+
+GtkWidget *web_view;
 GtkWidget *search_label;
 GtkWidget *search_entry;
 
+typedef struct{
+    char key;
+    char *name;
+    char *url;
+} Engine;
+
+Engine *engines = 0;
+int engine_count = 0;
+
 const char *search_url = 0;
+
+void add_engine(const char *arg){
+    if(!arg) return;
+    if(*arg == '\0') return;
+    char key = *arg;
+    arg++;
+    while(*arg == ' ') arg++;
+    int comma = 0;
+    while(arg[comma] != '\0' && arg[comma] != ',') comma++;
+    if(arg[comma] == '\0') return; // bad argument
+    engines = (Engine*) realloc(engines, sizeof(Engine) * (engine_count+1));
+    Engine *engine = engines + engine_count++;
+    engine->key = key;
+    engine->name = strndup(arg, comma);
+    arg += comma + 1;
+    while(*arg == ' ') arg++;
+    engine->url = strdup(arg);
+}
+
+Option options[] = {
+    { "search-engine", add_engine, "e.g. \"f Foo, http://foo/search?q=\"" },
+    { 0 }};
 
 void hide(){
     gtk_widget_hide(search_label);
@@ -16,9 +50,9 @@ void hide(){
     gtk_entry_set_text(GTK_ENTRY(search_entry), "");
 }
 
-void setup(const char *name, const char *url){
-    search_url = url;
-    gtk_label_set_text(GTK_LABEL(search_label), name);
+void setup(Engine *engine){
+    search_url = engine->url;
+    gtk_label_set_text(GTK_LABEL(search_label), engine->name);
     gtk_widget_show(search_label);
     gtk_widget_show(search_entry);
     gtk_widget_grab_focus(search_entry);
@@ -41,12 +75,12 @@ int key_callback(GdkEventKey *e){
     }
     else if(search_url)
         return FOCUS_GRAB | EVENT_PROPAGATE;
-#define ENGINE(K, E, U) case (K): setup((E),(U)); return FOCUS_GRAB | EVENT_STOP;
-    switch(e->keyval){
-        ENGINE(GDK_KEY_g, "Google", "http://google.com/search?hl=en&q=")
-        ENGINE(GDK_KEY_G, "Lucky?", "http://google.com/search?hl=en&btnI=&q=")
-        ENGINE(GDK_KEY_w, "Wikipedia", "http://en.wikipedia.org/wiki/")
-        ENGINE(GDK_KEY_W, "Wiktionary", "http://en.wiktionary.org/wiki/")
+    int i = 0;
+    for(; i<engine_count; i++){
+        if(engines[i].key == e->keyval){
+            setup(engines + i);
+            return FOCUS_GRAB | EVENT_STOP;
+        }
     }
     return EVENT_PROPAGATE;
 }
@@ -61,5 +95,5 @@ void init(Lumi *lumi){
     search_entry = gtk_entry_new();
     gtk_entry_set_has_frame(GTK_ENTRY(search_entry), FALSE);
     g_signal_connect(search_entry, "activate", G_CALLBACK(search), NULL);
-    gtk_box_pack_start(GTK_BOX(lumi->status_bar), search_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(lumi->status_bar), search_entry, TRUE, TRUE, 0);
 }
