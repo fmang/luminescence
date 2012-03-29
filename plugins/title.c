7340f837fb9f5526ccffbaad72bdd2e02ccad9d0
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-29 21:18:35 +0200

    'title' plugin

---
+++ plugins/title.c
@@ -0,0 +1,28 @@
+#include <luminescence.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *name = "Title";
+
+const char *description = "Updates the window's title.";
+
+GtkWidget *window;
+
+void set_title(WebKitWebView *web_view){
+    const char *title = webkit_web_view_get_title(web_view);
+    if(title){
+        char *window_title = (char*) malloc(strlen(title)+16);
+        strcpy(window_title, title);
+        strcat(window_title, " - Luminescence");
+        gtk_window_set_title(GTK_WINDOW(window), window_title);
+        free(window_title);
+    }
+    else
+        gtk_window_set_title(GTK_WINDOW(window), "Luminescence");
+}
+
+void init(Lumi *lumi){
+    window = lumi->window;
+    set_title(WEBKIT_WEB_VIEW(lumi->web_view));
+    g_signal_connect(lumi->web_view, "notify::title", G_CALLBACK(set_title), NULL);
+}
