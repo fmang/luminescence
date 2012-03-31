4d8ac111c0a87ff54391996b321f4b61ebdce7d4
Frédéric Mangano <fred.mangano@gmail.com>
2012-03-31 18:24:50 +0200

    more macros

--- luminescence.h
+++ luminescence.h
@@ -8,8 +8,8 @@ typedef struct {
     GtkWidget *window;     // GtkWindow
     GtkWidget *status_bar; // GtkBox
     GtkWidget *web_view;   // WebKitWebView
+
     void (*exec)(const char*, const char*);
-    // bind: modifier, key, command, argument
     void* (*bind)(guint, guint, const char*, const char*);
     void (*unbind)(void*);
 } Lumi;
@@ -20,7 +20,12 @@ typedef struct {
     const char *help;
 } Command;
 
-#define lumi_focus() (*lumi->exec)("focus", 0)
-#define lumi_leave() (*lumi->exec)("leave", 0)
+#define lumi_exec(cmd, arg) (*lumi->exec)((cmd), (arg))
+#define lumi_bind(key, cmd, arg) (*lumi->bind)(0, (key), (cmd), (arg))
+#define lumi_bind_mod(mod, key, cmd, arg) (*lumi->bind)((mod), (key), (cmd), (arg))
+#define lumi_unbind(b) (*lumi->unbind)((b))
+
+#define lumi_focus() lumi_exec("focus", 0)
+#define lumi_leave() lumi_exec("leave", 0)
 
 #endif // LUMINESCENCE_H
