0966f41ab856f9cea805ba13ddde84c7864309c5
Frédéric Mangano <fred.mangano@gmail.com>
2012-04-04 16:16:09 +0200

    updated 'exec' plugin with escaping and quoting

--- plugins/exec.c
+++ plugins/exec.c
@@ -4,76 +4,67 @@
 
 Lumi *lumi;
 
-void **bindings = 0;
-int binding_count;
-
-void run(char *line, int is_keys){
-    char *lf = strchr(line, '\n');
-    if(lf) *lf = '\0';
-    const char *cur = 0;
-    size_t count;
-    char *command;
-    guint key;
-    for(cur = line; *cur == ' '; cur++); // skip spaces
-    if(*cur == '\0') return; // null
-    if(*cur == '\n' || *cur == '#') return; // comment or empty
-    if(is_keys){
-        if(cur[1] != ' ') return; // only one-letter keys are allowed
-        key = *cur;
-        cur++;
-        while(*cur == ' ') cur++; // skip spaces
-    }
-    if(*cur == '\0') return;
-    for(count = 0; cur[count] != '\0' && cur[count] != ' '; count++);
-    command = strndup(cur, count);
-    cur += count;
-    while(*cur == ' ') cur++; // skip spaces
-    if(is_keys){
-        bindings = (void**) realloc(bindings, sizeof(void*) * (binding_count+1));
-        bindings[binding_count++] = lumi_bind(key, command, *cur == '\0' ? 0 : strdup(cur));
-    }
-    else{
-        lumi_exec(command, *cur == '\0' ? 0 : cur);
-        free(command);
-    }
+void push(int *argc, char ***argv, char *arg, int *cur){
+    arg[*cur] = '\0';
+    *cur = 0;
+    *argv = (char**) realloc(*argv, sizeof(char*) * (*argc+1));
+    (*argv)[(*argc)++] = strdup(arg);
 }
 
-void interpret(const char *file, bool is_bind){
+void exec(const char *c){
+    int argc = 0;
+    char **argv = 0;
+    char *arg = (char*) malloc(strlen(c)+1);
+    int cur = 0;
+    char quote = 0, escape = 0;
+    for(; *c != '\0' && *c != '\n'; c++){
+        if(escape){ escape = 0; arg[cur++] = *c; }
+        else if(*c == '\\') escape = *c;
+        else if(*c == quote){ quote = 0; push(&argc, &argv, arg, &cur); }
+        else if(quote) arg[cur++] = *c;
+        else if(*c == '#') break;
+        else if(*c == '"') quote = *c;
+        else if(*c == ' ' && cur) push(&argc, &argv, arg, &cur);
+        else if(*c != ' ') arg[cur++] = *c;
+    }
+    if(cur) push(&argc, &argv, arg, &cur);
+    free(arg);
+    if(!argv) return;
+    lumi_execv(argc, argv);
+    int i = 0;
+    for(; i<argc; i++)
+        free(argv[i]);
+    free(argv);
+}
+
+void exec_many(int argc, char **argv){
+    int i = 1;
+    for(; i<argc; i++)
+        exec(argv[i]);
+}
+
+void exec_file(const char *file){
     FILE *f = fopen(file, "r");
     if(!f) return;
     char *line = 0;
     size_t n = 0;
     while(getline(&line, &n, f) != -1)
-        run(line, is_bind);
+        exec(line);
     free(line);
     fclose(f);
 }
 
-void exec(const char *c){
-    char *line = strdup(c);
-    run(line, 0);
-    free(line);
-}
-
-void read_file(const char *f){ interpret(f, 0); }
-void read_keys(const char *f){ interpret(f, 1); }
-
-void unbind_keys(){
-    int i = 0;
-    for(; i<binding_count; i++)
-        lumi_unbind(bindings[i]);
-    free(bindings);
-    bindings = 0;
-    binding_count = 0;
+void exec_files(int argc, char **argv){
+    int i = 1;
+    for(; i<argc; i++)
+        exec_file(argv[i]);
 }
 
 Command commands[] = {
-    { "exec", exec },
-    { "run", read_file },
-    { "key-bindings", read_keys },
-    { "clear-bindings", unbind_keys },
+    { "exec", exec_many },
+    { "run", exec_files },
     { 0 } };
 
 void init(){
-    read_file("config");
+    exec_file("config");
 }
