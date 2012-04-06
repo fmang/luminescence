0784931ca0ddf4ec5baa56a8715c6f075b2bc61b
Frédéric Mangano <fred.mangano@gmail.com>
2012-04-06 18:14:07 +0200

    exec: multi-line

--- plugins/exec.c
+++ plugins/exec.c
@@ -12,29 +12,32 @@ void push(int *argc, char ***argv, char *arg, int *cur){
 }
 
 void exec(const char *c){
-    int argc = 0;
-    char **argv = 0;
-    char *arg = (char*) malloc(strlen(c)+1);
-    int cur = 0;
-    char quote = 0, escape = 0;
-    for(; *c != '\0' && *c != '\n'; c++){
-        if(escape){ escape = 0; arg[cur++] = *c; }
-        else if(*c == '\\') escape = *c;
-        else if(*c == quote){ quote = 0; push(&argc, &argv, arg, &cur); }
-        else if(quote) arg[cur++] = *c;
-        else if(*c == '#') break;
-        else if(*c == '"') quote = *c;
-        else if(*c == ' ' && cur) push(&argc, &argv, arg, &cur);
-        else if(*c != ' ') arg[cur++] = *c;
+    int argc, cur, i;
+    char **argv = 0, *arg = (char*) malloc(strlen(c)+1);
+    char quote, escape;
+    while(1){
+        while(*c == '\n' || *c == ' ') c++;
+        if(*c == '\0') break;
+        argc = cur = 0;
+        quote = escape = 0;
+        for(; *c != '\0' && *c != '\n'; c++){
+            if(escape){ escape = 0; arg[cur++] = *c; }
+            else if(*c == '\\') escape = *c;
+            else if(*c == quote){ quote = 0; push(&argc, &argv, arg, &cur); }
+            else if(quote) arg[cur++] = *c;
+            else if(*c == '#'){ while(*c != '\0' && *c != '\n') c++; }
+            else if(*c == '"') quote = *c;
+            else if(*c == ' ' && cur) push(&argc, &argv, arg, &cur);
+            else if(*c != ' ') arg[cur++] = *c;
+        }
+        if(cur) push(&argc, &argv, arg, &cur);
+        if(!argc) continue;
+        lumi_execv(argc, argv);
+        for(i=0; i<argc; i++)
+            free(argv[i]);
     }
-    if(cur) push(&argc, &argv, arg, &cur);
     free(arg);
-    if(!argv) return;
-    lumi_execv(argc, argv);
-    int i = 0;
-    for(; i<argc; i++)
-        free(argv[i]);
-    free(argv);
+    if(argv) free(argv);
 }
 
 void exec_many(int argc, char **argv){
