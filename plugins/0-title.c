ec6c23abd9994303ef8f9a40898c88bada5344ef
Frédéric Mangano <fred.mangano@gmail.com>
2012-04-25 13:07:21 +0200

    title: include pid

--- plugins/0-title.c
+++ plugins/0-title.c
@@ -1,25 +1,28 @@
 #include <luminescence.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 const char *name = "Window title";
 
 Lumi *lumi;
+char *suffix;
 
 void set_title(WebKitWebView *web_view){
     const char *title = webkit_web_view_get_title(WEBKIT_WEB_VIEW(lumi->web_view));
     if(title){
-        char *window_title = (char*) malloc(strlen(title)+16);
-        strcpy(window_title, title);
-        strcat(window_title, " - Luminescence");
+        char *window_title = (char*) malloc(strlen(title)+strlen(suffix)+4);
+        sprintf(window_title, "%s - %s", title, suffix);
         gtk_window_set_title(GTK_WINDOW(lumi->window), window_title);
         free(window_title);
     }
     else
-        gtk_window_set_title(GTK_WINDOW(lumi->window), "Luminescence");
+        gtk_window_set_title(GTK_WINDOW(lumi->window), suffix);
 }
 
 void init(){
+    suffix = (char*) malloc(32);
+    sprintf(suffix, "Luminescence <%d>", getpid());
     set_title(WEBKIT_WEB_VIEW(lumi->web_view));
     g_signal_connect(lumi->web_view, "notify::title", G_CALLBACK(set_title), NULL);
 }
