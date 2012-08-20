d500ccd76456d1b6cffd176775eec28fe0ce9ab0
Frédéric Mangano <fred.mangano@gmail.com>
2012-08-20 19:39:49 +0200

    scroll plugin: pageup / pagedown / top / bottom

--- plugins/0-scroll.c
+++ plugins/0-scroll.c
@@ -9,19 +9,22 @@ Lumi *lumi;
 
 void scroll(int argc, char **argv){
     if(argc <= 1) return;
-    GtkAdjustment *adjustment = argv[1][0] == 'u' || argv[1][0] == 'd'
-        ? gtk_scrollable_get_vadjustment(GTK_SCROLLABLE(lumi->web_view))
-        : gtk_scrollable_get_hadjustment(GTK_SCROLLABLE(lumi->web_view));
-    gdouble factor = 0;
-    ARG_CASE("up")    factor = -1;
-    ARG_CASE("right") factor = 1;
-    ARG_CASE("down")  factor = 1;
-    ARG_CASE("left")  factor = -1;
-    if(factor == 0) return;
-    gtk_adjustment_set_value(adjustment,
-        gtk_adjustment_get_value(adjustment) + factor*gtk_adjustment_get_step_increment(adjustment));
+    GtkAdjustment *adjustment = argv[1][0] == 'l' || argv[1][0] == 'r'
+        ? gtk_scrollable_get_hadjustment(GTK_SCROLLABLE(lumi->web_view))
+        : gtk_scrollable_get_vadjustment(GTK_SCROLLABLE(lumi->web_view));
+    gdouble value = gtk_adjustment_get_value(adjustment);
+    gdouble step = gtk_adjustment_get_step_increment(adjustment);
+    ARG_CASE("up")       value -= step;
+    ARG_CASE("right")    value += step;
+    ARG_CASE("down")     value += step;
+    ARG_CASE("left")     value -= step;
+    ARG_CASE("pageup")   value -= gtk_adjustment_get_page_increment(adjustment);
+    ARG_CASE("pagedown") value += gtk_adjustment_get_page_increment(adjustment);
+    ARG_CASE("top")      value = gtk_adjustment_get_lower(adjustment);
+    ARG_CASE("bottom")   value = gtk_adjustment_get_upper(adjustment);
+    gtk_adjustment_set_value(adjustment, value);
 }
 
 Command commands[] = {
-    { "scroll", scroll, "up / right / down / left" },
+    { "scroll", scroll, "up / right / down / left / pageup / pagedown / top / bottom" },
     { 0 } };
